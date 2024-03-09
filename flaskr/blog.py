3047f4ad9f9c7555b87a960f081ef05cf59b9014
Emaonide <emanuel.fonseca@correo.ucu.edu.uy>
2024-03-09 08:47:08 -0300

    Corrección de archivos que presentaban errores por no ejecutar pre-commit

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,5 +1,4 @@
-from flask import (Blueprint, flash, g, redirect, render_template, request,
-                   url_for)
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
--- flaskr/task.py
+++ flaskr/task.py
@@ -1,24 +1,23 @@
-import openai
-from flask import Blueprint, flash, request, jsonify
-from openai import OpenAI
-from flaskr.db import get_db
 import configparser
+
+import openai
+from flask import Blueprint, flash, jsonify, request
+from openai import OpenAI
+
+from flaskr.db import get_db
 
 bp = Blueprint("task", __name__, url_prefix="/task")  # Creates a blueprint named 'task'
 
 config = configparser.ConfigParser()
-config.read('../config.ini')
-openai.api_key = config['openai']['api_key']
+config.read("../config.ini")
+openai.api_key = config["openai"]["api_key"]
 
 
-# # Default rubric
-#default_rubric = "Eres un profesor de un curso de educación intermedia y debes corregir una tarea que te va a pasar el usuario en una escala de 0 a 100 donde 60 es el mínimo de aprobación. Además de la nota debes darle un concepto a la tarea."
 @bp.route("/correction", methods=["POST"])
 def text_CU():
     data = request.json
     task = data.get("task")
-    rubric= data.get("rubric")
-    #rubric = data.get('rubric', default_rubric)  # If empty, uses default rubric
+    rubric = data.get("rubric")
     if not task:
         return jsonify({"error": "The task text is required."}), 400
     if not rubric:
@@ -45,10 +44,6 @@ def text_CU():
         return jsonify({"error": str(e)}), 500
 
 
-
-
-
-
 @bp.route("/text", methods=["GET"])
 def text_correction():
     if request.method == "GET":
@@ -67,8 +62,6 @@ def text_correction():
                 return True
             except db.IntegrityError:
                 error = "Some errors happened during task processing. {}"
-            else:
-                return True
 
         flash(error)
 
